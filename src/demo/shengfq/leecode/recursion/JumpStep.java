6a6e9da916a1bbbde67ef5503eb8e7e170bc10a5
shengfq <1085748383@qq.com>
2020-02-13 11:21:59 +0800

    2020-02-13 shengfq 1.递归函数案例

---
+++ src/demo/shengfq/leecode/recursion/JumpStep.java
@@ -0,0 +1,16 @@
+package demo.shengfq.leecode.recursion;
+/**
+ * 递归调用
+ * 青蛙跳台阶
+ * **/
+public class JumpStep {
+    //1、第一递归函数功能
+    private static int f(int n){
+        //2、找出递归结束的条件
+        if(n<=2){
+            return n;
+        }
+        //3.找出函数的等价关系式
+        return f(n-1)+f(n-2);
+    }
+}
