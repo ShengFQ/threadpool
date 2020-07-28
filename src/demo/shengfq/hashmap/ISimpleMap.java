25d111037868428badb45e8d4d6b13a87d33d970
shengfq <1085748383@qq.com>
2020-07-28 14:23:42 +0800

    2020-07-28 shengfq 1.自定义HashMap实现类 2.Lambda表达式学习

---
+++ src/demo/shengfq/hashmap/ISimpleMap.java
@@ -0,0 +1,28 @@
+package demo.shengfq.hashmap;
+
+/**
+ * 简单的Map接口
+ *
+ * @author sheng
+ * @date 2020-07-28
+ * @copyright shengfq
+ */
+public interface ISimpleMap {
+
+    void put(Object key, Object value);
+
+    Object get(Object key);
+
+    int size();
+
+    boolean isEmpty();
+
+    /**
+     * 定义内部接口
+     */
+    interface Entry {
+        Object getKey();
+
+        Object getValue();
+    }
+}
---
+++ src/demo/shengfq/java8/feature/lambda/Apple.java
@@ -0,0 +1,35 @@
+package demo.shengfq.java8.feature.lambda;
+
+public class Apple  {
+    private String type;
+    private int weight;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Apple(String type, int weight) {
+        this.type = type;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Apple{" +
+                "type='" + type + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
